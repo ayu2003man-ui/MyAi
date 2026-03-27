d150a7b2bac5ef8317bac5e619b201cec832f932
ayu2003man-ui <ayu2003.man@gmail.com>
2026-03-27 23:52:05 +0530

    add backendapp link to forntend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
       return [...prev, { sender: "AI", text: "..." }];
     });
 
-    const res = await fetch("http://127.0.0.1:5000/chat", {
+    const res = await fetch("https://myai-izkb.onrender.com", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
